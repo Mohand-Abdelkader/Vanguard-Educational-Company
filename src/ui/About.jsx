6c229fc5a262b6cb8860ff3610d8dd616c2d2f26
Mohand Abdelkader <mohand27m@gmail.com>
2025-05-11 16:15:48 +0300

    fix production issue

--- src/ui/About.jsx
+++ src/ui/About.jsx
@@ -23,26 +23,7 @@ function About() {
               {t("about.paragraph2")}
             </p>
 
-            <div className="pt-4">
-              <a
-                href="#services"
-                className="inline-flex items-center text-secondary font-medium hover:text-secondary-dark transition-colors"
-              >
-                {t("about.discoverServices")}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 ml-1"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </a>
-            </div>
+            <div className="pt-4"></div>
           </div>
 
           <div className="bg-primary/5 dark:bg-primary/10 rounded-lg p-8">
